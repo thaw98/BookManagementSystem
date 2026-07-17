2ef9b37ea5f734ad54add68c50a5b97cc46c93b3
Pyae Phyo Thaw <ppthaw1998@gmail.com>
2026-07-17 14:35:01 +0630

    Setup Commit

---
+++ BookManagementSystem.Database/AppDbContextModels/AppDbContextFactory.cs
@@ -0,0 +1,28 @@
+﻿using Database.AppDbContextModels;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Design;
+
+namespace BookManagementSystem.Database.AppDbContextModels;
+
+public class AppDbContextFactory
+    : IDesignTimeDbContextFactory<AppDbContext>
+{
+    public AppDbContext CreateDbContext(string[] args)
+    {
+        const string connectionString =
+            "server=localhost;" +
+            "port=3306;" +
+            "database=book_management_system;" +
+            "user=root;" +
+            "password=root;";
+
+        var optionsBuilder =
+            new DbContextOptionsBuilder<AppDbContext>();
+
+        optionsBuilder.UseMySql(
+            connectionString,
+            new MySqlServerVersion(new Version(8, 0, 32)));
+
+        return new AppDbContext(optionsBuilder.Options);
+    }
+}
